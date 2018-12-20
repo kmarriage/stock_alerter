f479b1386bc75a231ea87d0f5ede277791db9fd0
Ken Marriage <ken.marriage@ihsmarkit.com>
2018-12-20 07:25:47 -0700

    Changed tabs to spaces

--- tests/test_stock.py
+++ tests/test_stock.py
@@ -3,6 +3,6 @@ from ..stock import Stock
 
 
 class StockTest(unittest.TestCase):
-	def test_price_of_a_new_stock_class_should_be_None(self):
-		stock = Stock('GOOG')
-		self.assertIsNone(stock.price)
+    def test_price_of_a_new_stock_class_should_be_None(self):
+        stock = Stock('GOOG')
+        self.assertIsNone(stock.price)
