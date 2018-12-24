87f8ce5ad3c00140817c9d1780704880ed29b782
Ken Marriage <ken.marriage@ihsmarkit.com>
2018-12-24 11:45:33 -0700

    PEP8 refactoring

--- stock.py
+++ stock.py
@@ -31,7 +31,8 @@ class Stock:
         self.history.update(timestamp, price)
 
     def is_increasing_trend(self):
-        return self.history[-3].value < self.history[-2].value < self.history[-1].value
+        return self.history[-3].value < self.history[-2].value < \
+               self.history[-1].value
 
     def _get_closing_price_list(self, on_date, num_days):
         closing_price_list = []
@@ -50,24 +51,34 @@ class Stock:
 
     def _is_crossover_below_to_above(self, prev_ma, prev_reference_ma,
                                      current_ma, current_reference_ma):
-        return prev_ma < prev_reference_ma and current_ma > current_reference_ma
+        return prev_ma < prev_reference_ma and current_ma > \
+               current_reference_ma
 
     def get_crossover_signal(self, on_date):
         NUM_DAYS = self.LONG_TERM_TIMESPAN + 1
-        closing_price_list = self.history.get_closing_price_list(on_date, NUM_DAYS)
+        closing_price_list = self.history.get_closing_price_list(on_date,
+                                                                 NUM_DAYS)
 
         if len(closing_price_list) < NUM_DAYS:
             return StockSignal.neutral
 
-        long_term_series = closing_price_list[-self.LONG_TERM_TIMESPAN:]
-        prev_long_term_series = closing_price_list[-self.LONG_TERM_TIMESPAN-1:-1]
-        short_term_series = closing_price_list[-self.SHORT_TERM_TIMESPAN:]
-        prev_short_term_series = closing_price_list[-self.SHORT_TERM_TIMESPAN-1:-1]
+        long_term_series = \
+            closing_price_list[-self.LONG_TERM_TIMESPAN:]
+        prev_long_term_series = \
+            closing_price_list[-self.LONG_TERM_TIMESPAN-1:-1]
+        short_term_series = \
+            closing_price_list[-self.SHORT_TERM_TIMESPAN:]
+        prev_short_term_series = \
+            closing_price_list[-self.SHORT_TERM_TIMESPAN-1:-1]
 
-        long_term_ma = sum([update.value for update in long_term_series]) / self.LONG_TERM_TIMESPAN
-        prev_long_term_ma = sum([update.value for update in prev_long_term_series]) / self.LONG_TERM_TIMESPAN
-        short_term_ma = sum([update.value for update in short_term_series]) / self.SHORT_TERM_TIMESPAN
-        prev_short_term_ma = sum([update.value for update in prev_short_term_series]) / self.SHORT_TERM_TIMESPAN
+        long_term_ma = sum([update.value for update in long_term_series]) / \
+                       self.LONG_TERM_TIMESPAN
+        prev_long_term_ma = sum([update.value for update in prev_long_term_series]) / \
+                            self.LONG_TERM_TIMESPAN
+        short_term_ma = sum([update.value for update in short_term_series]) / \
+                        self.SHORT_TERM_TIMESPAN
+        prev_short_term_ma = sum([update.value for update in prev_short_term_series]) / \
+                             self.SHORT_TERM_TIMESPAN
 
         if self._is_crossover_below_to_above(prev_short_term_ma, prev_long_term_ma,
                                              short_term_ma, long_term_ma):
