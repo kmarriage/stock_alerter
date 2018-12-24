e1dd937a473febf0a38982fc7eefd1de2cfed246
Ken Marriage <ken.marriage@ihsmarkit.com>
2018-12-24 09:25:12 -0700

    refactor to take timestamp as a sort for updates

--- tests/test_stock.py
+++ tests/test_stock.py
@@ -7,18 +7,6 @@ class StockTest(unittest.TestCase):
     def setUp(self):
         self.goog = Stock('GOOG')
 
-    def test_increasing_trend_is_true_if_price_increase_for_3_updates(self):
-        self.given_a_series_of_prices([8, 10, 12])
-        self.assertTrue(self.goog.is_increasing_trend())
-
-    def test_increasing_trend_is_false_if_price_decreases(self):
-        self.given_a_series_of_prices([8, 12, 10])
-        self.assertFalse(self.goog.is_increasing_trend())
-
-    def test_increasing_trend_is_false_if_price_equal(self):
-        self.given_a_series_of_prices([8, 10, 10])
-        self.assertFalse(self.goog.is_increasing_trend())
-
     def test_price_of_a_new_stock_class_should_be_None(self):
         self.assertIsNone(self.goog.price)
 
@@ -39,9 +27,31 @@ class StockTest(unittest.TestCase):
         self.goog.update(datetime(2014, 2, 13), price=8.4)
         self.assertAlmostEqual(8.4, self.goog.price, delta=0.0001)
 
+    def test_price_is_the_latest_even_if_updates_are_made_out_of_order(self):
+        self.goog.update(datetime(2014, 2, 13), price=8)
+        self.goog.update(datetime(2014, 2, 12), price=10)
+        self.assertEqual(8, self.goog.price)
+
+
+class StockTrendTest(unittest.TestCase):
+    def setUp(self):
+        self.goog = Stock("GOOG")
+
     def given_a_series_of_prices(self, prices):
         timestamps = [datetime(2014, 2, 11),
                       datetime(2014, 2, 12),
                       datetime(2014, 2, 13)]
         for timestamp, price in zip(timestamps, prices):
             self.goog.update(timestamp, price)
+
+    def test_increasing_trend_is_true_if_price_increase_for_3_updates(self):
+        self.given_a_series_of_prices([8, 10, 12])
+        self.assertTrue(self.goog.is_increasing_trend())
+
+    def test_increasing_trend_is_false_if_price_decreases(self):
+        self.given_a_series_of_prices([8, 12, 10])
+        self.assertFalse(self.goog.is_increasing_trend())
+
+    def test_increasing_trend_is_false_if_price_equal(self):
+        self.given_a_series_of_prices([8, 10, 10])
+        self.assertFalse(self.goog.is_increasing_trend())
