a38748d422409058e00bf24a36bbb64ef4dc02fb
Ken Marriage <ken.marriage@ihsmarkit.com>
2018-12-26 16:08:59 -0700

    Implemented the use of unittest mock

--- tests/test_event.py
+++ tests/test_event.py
@@ -1,28 +1,29 @@
 import unittest
 from ..event import Event
+from unittest import mock
 
 
 class EventTest(unittest.TestCase):
     def test_a_listener_is_notified_when_an_event_is_raised(self):
-        listener = Mock()
+        listener = mock.Mock()
         event = Event()
         event.connect(listener)
         event.fire()
         self.assertTrue(listener.called)
 
     def test_a_listener_is_passed_right_parameters(self):
-        listener = Mock()
+        listener = mock.Mock()
         event = Event()
         event.connect(listener)
         event.fire(5, shape='square')
-        self.assertEquals(((5, ), {'shape': 'square'}), listener.params)
+        listener.assert_called_with(5, shape='square')
 
-
-class Mock:
-    def __init__(self):
-        self.called = False
-        self.params = ()
-
-    def __call__(self, *args, **kwargs):
-        self.called = True
-        self.params = (args, kwargs)
+#
+# class Mock:
+#     def __init__(self):
+#         self.called = False
+#         self.params = ()
+#
+#     def __call__(self, *args, **kwargs):
+#         self.called = True
+#         self.params = (args, kwargs)
